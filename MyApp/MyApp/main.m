aa278fc5e678da5d32cafcf74fb6a2f2ba251a41
Tui Coding <tuicoding@gmail.com>
2015-06-19 14:45:50 +0700

    Basic syntax

--- MyApp/MyApp/main.m
+++ MyApp/MyApp/main.m
@@ -11,8 +11,16 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
-        int x = 10;
+        int days = 365;
+        int hours = 24;
+        int minutes = 60;
+
+        int minutesInAYear = days * hours * minutes;
+
+        NSLog(@"Có %i phút trong một năm", minutesInAYear);
+        
+        NSLog(@"Một năm có %i ngày, mỗi ngày có %i giờ,"
+              " mỗi giờ có %i phút trong một năm", days, hours, minutes);
     }
     return 0;
 }
