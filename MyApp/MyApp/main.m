df40022fc8f182940fff058f64f5a80f8f15b619
Tui Coding <tuicoding@gmail.com>
2015-06-19 10:52:18 +0700

    Init

---
+++ MyApp/MyApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyApp
+//
+//  Created by Tui Coding on 6/19/15.
+//  Copyright (c) 2015 TuiCoding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
